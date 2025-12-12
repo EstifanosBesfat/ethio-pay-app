79932a5297cb00e856d4e30c9b6e2b602256d07a
EstifanosBesfat <besfatestifanos@gmail.com>
2025-12-12 00:09:15 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import SendMoneyCard from "@/components/send-money-card"
+// (Note: VS Code might suggest the import automatically)
+
+function App() {
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center bg-gray-100 p-4">
+      <SendMoneyCard />
+    </div>
+  )
+}
+
+export default App
